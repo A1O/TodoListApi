eee770ab2c355ba5f88b07fade2f37172ec13e27
Domas Pozniakas <dpozniakas@pop-os.localdomain>
2021-01-21 11:48:31 +0200

    small fix

--- src/ExpressServer/Middleware/authenticateJWT.ts
+++ src/ExpressServer/Middleware/authenticateJWT.ts
@@ -8,7 +8,7 @@ const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
     const secretKey = <string>process.env.JWT_SECRET_KEY;
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, secretKey, (err, user) => {
+    jwt.verify(token, secretKey, (err, payload) => {
       if (err) {
         return res.sendStatus(401);
       }
