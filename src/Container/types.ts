7cbaca6c0a3970a896975ed6e614d1a672c9ac78
Domas Pozniakas <pozniakas@pop-os.localdomain>
2021-01-28 15:07:19 +0200

    Removed unused dependency types

--- src/Container/types.ts
+++ src/Container/types.ts
@@ -1,13 +1,6 @@
 export const DependencyTypes = {
-  ITodoListApi: Symbol.for('ITodoListApi'),
-  IExpressServer: Symbol.for('IExpressServer'),
-  IDatabase: Symbol.for('IDatabase'),
   IUserRepository: Symbol.for('IUserRepository'),
   IUserService: Symbol.for('IUserService'),
-  IServices: Symbol.for('IServices'),
   IJsonWebToken: Symbol.for('IJsonWebToken'),
-  IUserController: Symbol.for('IUserController'),
-  ITaskController: Symbol.for('ITaskController'),
-  IControllers: Symbol.for('IControllers'),
   ITaskService: Symbol.for('ITaskService'),
 };
